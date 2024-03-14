f524bb22c106d55023012cb4e69b4edcd708a244
pprahas <p.prahas@protonmail.com>
2024-03-14 12:27:48 -0500

    cleaned up factory pattern before implementing inversify

--- backend/src/domain/IReportGeneratorFactory.ts
+++ backend/src/domain/IReportGeneratorFactory.ts
@@ -9,13 +9,10 @@ enum Types {
 }
 
 interface IReportGeneratorFactory {
-  //TODO
-  // reportTypeMap: Record<Types, new () => IReportGenerator>;
   create(value: string): IReportGenerator;
 }
 
 export class ReportGeneratorFactory implements IReportGeneratorFactory {
-  //TODO - put nothing in the constructor
   constructor() {}
 
   reportTypeMap: Record<Types, IReportGenerator> = {
@@ -26,19 +23,5 @@ export class ReportGeneratorFactory implements IReportGeneratorFactory {
   create(type: string): IReportGenerator {
     const typeEnum = Types[type as keyof typeof Types];
     return this.reportTypeMap[typeEnum];
-    console.log(typeEnum);
-
-    // if (!typeEnum) {
-    //   return { error: "Invalid report type", code: 400 };
-    // }
-
-    // const ReportClass = this.reportTypeMap[typeEnum];
-    // console.log(ReportClass);
-    // if (!ReportClass) {
-    //   return { error: "Report type not supported", code: 400 };
-    // }
-
-    // const reportInstance = new ReportClass();
-    // return { content: reportInstance.reportGenerate(), code: 200 };
   }
 }
--- backend/src/endpoints/controllers/report/getReport.ts
+++ backend/src/endpoints/controllers/report/getReport.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { ReportService } from "../../../database/report/Get/getReportContent";
 import { ReportGeneratorFactory } from "../../../domain/IReportGeneratorFactory";
 export async function getReport(req: Request, res: Response): Promise<any> {
   try {
@@ -11,18 +10,13 @@ export async function getReport(req: Request, res: Response): Promise<any> {
         .send("Report type is required as a query parameter.");
     }
 
-    // let reportService = new ReportService();
-
-    var reportFactory = new ReportGeneratorFactory();
+    let reportFactory = new ReportGeneratorFactory();
 
     let reportGenerator = reportFactory.create(type);
 
     let report = reportGenerator.reportGenerate();
 
-    // let result = reportService.exportReport(type);
-
     return res.status(200).send(report);
-    // return res.status(result.code).send(result.content || result.error);
   } catch (error) {
     throw error;
   }
